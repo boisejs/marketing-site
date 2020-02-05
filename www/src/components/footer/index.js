03d39e4e1a23c89e81432c21d4a0655abdc3c130
Kevin E <notmyuwmail@gmail.com>
2020-02-04 22:02:24 -0800

    footer fix

--- www/src/components/footer/index.js
+++ www/src/components/footer/index.js
@@ -1,20 +1,19 @@
-import { Link } from 'gatsby'
-import React from 'react'
-import './style.scss'
+import { Link } from "gatsby";
+import React from "react";
+import "./style.scss";
 
 const Footer = ({ author, title }) => (
   <div className="footer">
     <div className="container">
       <hr className="border-primary" />
       <p>
-        {title}
-        <Link to="/profile/">
-          <br />
-          <strong>{author}</strong> on Profile
-        </Link>
+        Join us on{" "}
+        <a href="https://github.com/boisejs/" target="_blank">
+          <strong>on GitHub</strong>
+        </a>
       </p>
     </div>
   </div>
-)
+);
 
-export default Footer
+export default Footer;
