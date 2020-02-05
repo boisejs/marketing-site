68a34e8a06cdcc936e763aa72b3cd05248324843
Kevin E <notmyuwmail@gmail.com>
2020-02-04 21:23:54 -0800

    members page

--- www/src/pages/members.js
+++ www/src/pages/members.js
@@ -5,8 +5,56 @@ const Members = ({ data, location }) => {
   return (
     <Layout location={location}>
       <div className="container">
-        <h1 className="heading-1">Members</h1>
-        <h2 className="heading-2">UNDER CONSTRUCTION</h2>
+        <br />
+        <h1 className="heading-1 text-center text-primary">Coming Soon...</h1>
+        <br />
+        <div className="container">
+          <form name="contact" method="POST" data-netlify="true">
+            <legend className="text-center">
+              ... but you should sign up for updates!
+            </legend>
+            <br />
+            <fieldset>
+              <div class="form-group row required">
+                <div className="col-6 mx-auto">
+                  <label for="name">Your Name</label>
+                  <input
+                    name="name"
+                    type="text"
+                    class="form-control"
+                    id="name"
+                    aria-describedby="nameHelp"
+                    placeholder="Enter name"
+                  />
+                  <small id="nameHelp" class="form-text text-muted">
+                    A name you'd want printed on a name badge.
+                  </small>
+                </div>
+              </div>
+              <div class="form-group row required">
+                <div className="col-6 mx-auto">
+                  <label for="email">Email address</label>
+                  <input
+                    name="email"
+                    type="email"
+                    class="form-control"
+                    id="email"
+                    aria-describedby="emailHelp"
+                    placeholder="Enter email"
+                  />
+                  <small id="emailHelp" class="form-text text-muted">
+                    We'll never share your email with anyone else.
+                  </small>
+                </div>
+              </div>
+              <p className="row">
+                <button type="submit" className="btn btn-primary mx-auto">
+                  Sign Up
+                </button>
+              </p>
+            </fieldset>
+          </form>
+        </div>
       </div>
     </Layout>
   );
